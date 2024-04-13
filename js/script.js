d6579cfde46bf3a391bd8d831d1cfa3dd098170b
Danil <stepanovdanpav@gmail.com>
2024-04-13 14:48:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//alert('Hello');
+
+//const result = confirm('Are you here?');
+///console.log(result);
+
+//const answer = prompt('Вам есть 18?', '');
+//console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Как вас зовут?', '');
+answers[1] = prompt('Какая у вас фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
